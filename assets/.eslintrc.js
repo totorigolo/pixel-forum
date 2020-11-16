7a7ff7a0cefc77c124b4f672f6a241b5602cc168
Thomas Lacroix <toto.rigolo@free.fr>
2020-11-16 23:48:47 +0100

    More TypeScript linting.

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -1,11 +1,28 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  rules: {
+    "no-extra-semi": "off",
+    "@typescript-eslint/semi": ["error"],
+    "@typescript-eslint/no-extra-semi": ["error"],
+    "brace-style": "off",
+    "@typescript-eslint/brace-style": ["error"],
+    "no-loss-of-precision": "off",
+    "@typescript-eslint/no-loss-of-precision": ["error"],
+    "space-infix-ops": "off",
+    "@typescript-eslint/space-infix-ops": ["error", { "int32Hint": false }],
+    "@typescript-eslint/no-misused-new": "error"
+  }
 };
--- assets/js/image_socket.ts
+++ assets/js/image_socket.ts
@@ -7,17 +7,19 @@ const user_token = user_token_meta && user_token_meta.content || null;
 const socket = new Socket("/msgpack-socket", {
   params: { user_token: user_token },
   // logger: (kind, msg, data) => console.log(`${kind}: ${msg}`, data),
-  decode: (packed_payload: string, callback: (decoded: any) => void) => {
-    const decoded = msgpack.decode(new Uint8Array(packed_payload as unknown as ArrayBuffer));
+  decode: (packed_payload: string, callback: <T>(decoded: T) => void) => {
+    type PhxMessage = [string, string, string, string, unknown];
+
+    const decoded: PhxMessage = msgpack.decode(new Uint8Array(packed_payload as unknown as ArrayBuffer)) as PhxMessage;
     const [join_ref, ref, topic, event, payload] = decoded;
     return callback({ join_ref, ref, topic, event, payload });
   },
-})
+});
 
-socket.connect()
+socket.connect();
 
-const channel = socket.channel("image:1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2", {})
-const presence = new Presence(channel)
+const channel = socket.channel("image:1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2", {});
+const presence = new Presence(channel);
 
 //------------------------------------------------------------------------------
 // Canvas
@@ -29,13 +31,13 @@ imageCanvasCtx.imageSmoothingEnabled = false; // disable anti-aliasing
 function loadImage() {
   const image = new Image();
   image.onload = function () {
-    createImageBitmap(image, 0, 0, 512, 512).then(bitmap => imageCanvasCtx.drawImage(bitmap, 0, 0));
-  }
+    void createImageBitmap(image, 0, 0, 512, 512).then(bitmap => imageCanvasCtx.drawImage(bitmap, 0, 0));
+  };
   image.src = '/lobby/1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2/image';
 }
 
 function drawPixel(x: number, y: number, r: number, g: number, b: number) {
-  imageCanvasCtx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+  imageCanvasCtx.fillStyle = `rgb(${r},${g},${b})`;
   imageCanvasCtx.fillRect(x, y, 1, 1);
 }
 
@@ -44,13 +46,13 @@ loadImage();
 //------------------------------------------------------------------------------
 // Pixel updates
 
-const inputX: HTMLInputElement = document.querySelector("#input-x")
-const inputY: HTMLInputElement = document.querySelector("#input-y")
-const inputR: HTMLInputElement = document.querySelector("#input-r")
-const inputG: HTMLInputElement = document.querySelector("#input-g")
-const inputB: HTMLInputElement = document.querySelector("#input-b")
+const inputX: HTMLInputElement = document.querySelector("#input-x");
+const inputY: HTMLInputElement = document.querySelector("#input-y");
+const inputR: HTMLInputElement = document.querySelector("#input-r");
+const inputG: HTMLInputElement = document.querySelector("#input-g");
+const inputB: HTMLInputElement = document.querySelector("#input-b");
 
-const drawBtn = document.querySelector("#draw-btn")
+const drawBtn = document.querySelector("#draw-btn");
 drawBtn.addEventListener("click", _event => {
   channel.push("change_pixel", {
     x: inputX.value,
@@ -61,8 +63,8 @@ drawBtn.addEventListener("click", _event => {
   })
     .receive("ok", () => console.log("Pixel changed successfully."))
     .receive("error", () => console.error("Failed to change pixel."))
-    .receive("timeout", () => console.log("Timed out changing pixel"))
-})
+    .receive("timeout", () => console.log("Timed out changing pixel"));
+});
 
 function getUint40(dataView: DataView, byteOffset: number): number {
   const left = dataView.getUint8(byteOffset);
@@ -70,39 +72,39 @@ function getUint40(dataView: DataView, byteOffset: number): number {
   return 2 ** 32 * left + right;
 }
 
-channel.on("pixel_batch", payload => {
-  const binary_view = new DataView(payload.d.buffer, payload.d.byteOffset, payload.d.byteLength)
-  const start_version = getUint40(binary_view, 0)
-  const nb_changes = (binary_view.byteLength - 5) / (2 * 2 + 3 * 1)
+channel.on("pixel_batch", (payload: { d: Uint8Array }) => {
+  const binary_view = new DataView(payload.d.buffer, payload.d.byteOffset, payload.d.byteLength);
+  const start_version = getUint40(binary_view, 0);
+  const nb_changes = (binary_view.byteLength - 5) / (2 * 2 + 3 * 1);
   for (let i = 0; i < nb_changes; i++) {
     const o = 5 + i * 7;
-    const x = binary_view.getUint16(o)
-    const y = binary_view.getUint16(o + 2)
-    const r = binary_view.getUint8(o + 4)
-    const g = binary_view.getUint8(o + 5)
-    const b = binary_view.getUint8(o + 6)
-    drawPixel(x, y, r, g, b)
+    const x = binary_view.getUint16(o);
+    const y = binary_view.getUint16(o + 2);
+    const r = binary_view.getUint8(o + 4);
+    const g = binary_view.getUint8(o + 5);
+    const b = binary_view.getUint8(o + 6);
+    drawPixel(x, y, r, g, b);
   }
-})
+});
 
 //------------------------------------------------------------------------------
 // Presence
 
-const nbConnectedUsers = document.querySelector("#nb-connected-image")
+const nbConnectedUsers = document.querySelector("#nb-connected-image");
 
 function renderOnlineUsers(presence: Presence) {
   nbConnectedUsers.innerHTML = presence.list().length.toString();
 }
 
-channel.on("presence", payload => {
+channel.on("presence", (payload: { nb_connected: number }) => {
   nbConnectedUsers.innerHTML = JSON.stringify(payload.nb_connected, null, 2);
-})
-presence.onSync(() => renderOnlineUsers(presence))
+});
+presence.onSync(() => renderOnlineUsers(presence));
 
 //------------------------------------------------------------------------------
 
 channel.join()
-  .receive("ok", resp => { console.log("Joined successfully.") })
-  .receive("error", resp => { console.error("Unable to join: ", resp) })
+  .receive("ok", () => console.log("Joined successfully."))
+  .receive("error", resp => console.error("Unable to join: ", resp));
 
-export default socket
+export default socket;
