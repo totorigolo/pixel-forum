2bc768216bc714f72ed526e0fb2ce46de68fb448
Thomas Lacroix <toto.rigolo@free.fr>
2020-11-16 23:16:22 +0100

    Add ESLint.

--- assets/js/image_socket.ts
+++ assets/js/image_socket.ts
@@ -1,42 +1,42 @@
 import { Socket, Presence } from "phoenix";
 import msgpack from "./msgpack";
 
-let user_token_meta: HTMLMetaElement = document.querySelector('meta[name="user_ws_token"]');
-let user_token = user_token_meta && user_token_meta.content || null;
+const user_token_meta: HTMLMetaElement = document.querySelector('meta[name="user_ws_token"]');
+const user_token = user_token_meta && user_token_meta.content || null;
 
-let socket = new Socket("/msgpack-socket", {
-    params: { user_token: user_token },
-    // logger: (kind, msg, data) => console.log(`${kind}: ${msg}`, data),
-    decode: (packed_payload: string, callback: (decoded: any) => void) => {
-        let decoded = msgpack.decode(new Uint8Array(packed_payload as unknown as ArrayBuffer));
-        let [join_ref, ref, topic, event, payload] = decoded;
-        return callback({ join_ref, ref, topic, event, payload });
-    },
+const socket = new Socket("/msgpack-socket", {
+  params: { user_token: user_token },
+  // logger: (kind, msg, data) => console.log(`${kind}: ${msg}`, data),
+  decode: (packed_payload: string, callback: (decoded: any) => void) => {
+    const decoded = msgpack.decode(new Uint8Array(packed_payload as unknown as ArrayBuffer));
+    const [join_ref, ref, topic, event, payload] = decoded;
+    return callback({ join_ref, ref, topic, event, payload });
+  },
 })
 
 socket.connect()
 
-let channel = socket.channel("image:1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2", {})
-let presence = new Presence(channel)
+const channel = socket.channel("image:1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2", {})
+const presence = new Presence(channel)
 
 //------------------------------------------------------------------------------
 // Canvas
 
-let imageCanvas: HTMLCanvasElement = document.querySelector("#image-canvas");
-let imageCanvasCtx = imageCanvas.getContext('2d');
+const imageCanvas: HTMLCanvasElement = document.querySelector("#image-canvas");
+const imageCanvasCtx = imageCanvas.getContext('2d');
 imageCanvasCtx.imageSmoothingEnabled = false; // disable anti-aliasing
 
 function loadImage() {
-    let image = new Image();
-    image.onload = function () {
-        createImageBitmap(image, 0, 0, 512, 512).then(bitmap => imageCanvasCtx.drawImage(bitmap, 0, 0));
-    }
-    image.src = '/lobby/1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2/image';
+  const image = new Image();
+  image.onload = function () {
+    createImageBitmap(image, 0, 0, 512, 512).then(bitmap => imageCanvasCtx.drawImage(bitmap, 0, 0));
+  }
+  image.src = '/lobby/1f12b09f-21e5-4d7e-bc7e-cb8d75dc3ce2/image';
 }
 
 function drawPixel(x: number, y: number, r: number, g: number, b: number) {
-    imageCanvasCtx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    imageCanvasCtx.fillRect(x, y, 1, 1);
+  imageCanvasCtx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+  imageCanvasCtx.fillRect(x, y, 1, 1);
 }
 
 loadImage();
@@ -44,51 +44,51 @@ loadImage();
 //------------------------------------------------------------------------------
 // Pixel updates
 
-let inputX: HTMLInputElement = document.querySelector("#input-x")
-let inputY: HTMLInputElement = document.querySelector("#input-y")
-let inputR: HTMLInputElement = document.querySelector("#input-r")
-let inputG: HTMLInputElement = document.querySelector("#input-g")
-let inputB: HTMLInputElement = document.querySelector("#input-b")
+const inputX: HTMLInputElement = document.querySelector("#input-x")
+const inputY: HTMLInputElement = document.querySelector("#input-y")
+const inputR: HTMLInputElement = document.querySelector("#input-r")
+const inputG: HTMLInputElement = document.querySelector("#input-g")
+const inputB: HTMLInputElement = document.querySelector("#input-b")
 
-let drawBtn = document.querySelector("#draw-btn")
-drawBtn.addEventListener("click", event => {
-    channel.push("change_pixel", {
-        x: inputX.value,
-        y: inputY.value,
-        r: inputR.value,
-        g: inputG.value,
-        b: inputB.value,
-    })
-        .receive("ok", () => console.log("Pixel changed successfully."))
-        .receive("error", () => console.error("Failed to change pixel."))
-        .receive("timeout", () => console.log("Timed out changing pixel"))
+const drawBtn = document.querySelector("#draw-btn")
+drawBtn.addEventListener("click", _event => {
+  channel.push("change_pixel", {
+    x: inputX.value,
+    y: inputY.value,
+    r: inputR.value,
+    g: inputG.value,
+    b: inputB.value,
+  })
+    .receive("ok", () => console.log("Pixel changed successfully."))
+    .receive("error", () => console.error("Failed to change pixel."))
+    .receive("timeout", () => console.log("Timed out changing pixel"))
 })
 
 function getUint40(dataView: DataView, byteOffset: number): number {
-    const left = dataView.getUint8(byteOffset);
-    const right = dataView.getUint32(byteOffset + 1);
-    return 2 ** 32 * left + right;
+  const left = dataView.getUint8(byteOffset);
+  const right = dataView.getUint32(byteOffset + 1);
+  return 2 ** 32 * left + right;
 }
 
 channel.on("pixel_batch", payload => {
-    let binary_view = new DataView(payload.d.buffer, payload.d.byteOffset, payload.d.byteLength)
-    const start_version = getUint40(binary_view, 0)
-    const nb_changes = (binary_view.byteLength - 5) / (2 * 2 + 3 * 1)
-    for (let i = 0; i < nb_changes; i++) {
-        const o = 5 + i * 7;
-        const x = binary_view.getUint16(o)
-        const y = binary_view.getUint16(o + 2)
-        const r = binary_view.getUint8(o + 4)
-        const g = binary_view.getUint8(o + 5)
-        const b = binary_view.getUint8(o + 6)
-        drawPixel(x, y, r, g, b)
-    }
+  const binary_view = new DataView(payload.d.buffer, payload.d.byteOffset, payload.d.byteLength)
+  const start_version = getUint40(binary_view, 0)
+  const nb_changes = (binary_view.byteLength - 5) / (2 * 2 + 3 * 1)
+  for (let i = 0; i < nb_changes; i++) {
+    const o = 5 + i * 7;
+    const x = binary_view.getUint16(o)
+    const y = binary_view.getUint16(o + 2)
+    const r = binary_view.getUint8(o + 4)
+    const g = binary_view.getUint8(o + 5)
+    const b = binary_view.getUint8(o + 6)
+    drawPixel(x, y, r, g, b)
+  }
 })
 
 //------------------------------------------------------------------------------
 // Presence
 
-let nbConnectedUsers = document.querySelector("#nb-connected-image")
+const nbConnectedUsers = document.querySelector("#nb-connected-image")
 
 function renderOnlineUsers(presence: Presence) {
   nbConnectedUsers.innerHTML = presence.list().length.toString();
