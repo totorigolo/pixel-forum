f7e5589d743433889e1a9009f80e2899dfcc5381
Thomas Lacroix <toto.rigolo@free.fr>
2021-02-10 21:41:44 +0100

    LobbyManager retries to start all lobbies
    This is useful when deploying as containers when the database is not yet
    configured, as otherwise the application just crashes immediately, not
    allowing to run migrations or configure the DB.

--- lib/pixel_forum/forum/lobby_manager.ex
+++ lib/pixel_forum/forum/lobby_manager.ex
@@ -33,10 +33,13 @@ defmodule PixelForum.Forum.LobbyManager do
     Lobbies.subscribe()
     Logger.info("Lobby registry subscribed to \"lobbies\" topic.")
 
-    start_all_lobbies!()
-    Logger.info("Spawned lobby supervisors.")
+    {:ok, nil, {:continue, :start_all_lobbies}}
+  end
 
-    {:ok, nil}
+  @impl true
+  def handle_continue(:start_all_lobbies, state) do
+    Process.send_after(self(), :start_all_lobbies, 0)
+    {:noreply, state}
   end
 
   @impl true
@@ -72,17 +75,39 @@ defmodule PixelForum.Forum.LobbyManager do
   def handle_info({:lobby_image_reset, _lobby}, state), do: {:noreply, state}
 
   ##############################################################################
+  ## Private messages
+
+  @impl true
+  def handle_info(:start_all_lobbies, state) do
+    Logger.info("Spawning lobby supervisors...")
+
+    case start_all_lobbies() do
+      :ok ->
+        Logger.info("Spawned lobby supervisors.")
+
+      :error ->
+        Logger.error("Failed to spawn lobby supervisors, retrying in 10 seconds.")
+        Process.send_after(self(), :start_all_lobbies, 10 * 1000)
+    end
+
+    {:noreply, state}
+  end
+
+  ##############################################################################
   ## Private functions
 
   defp start_lobby_(lobby_id) do
-    result = DynamicSupervisor.start_child(Forum.LobbySupervisor, {Lobbies.LobbySupervisor, lobby_id})
+    result =
+      DynamicSupervisor.start_child(Forum.LobbySupervisor, {Lobbies.LobbySupervisor, lobby_id})
 
     case result do
       {:ok, pid} ->
-        Logger.info("Started lobby supervisor for #{lobby_id} at #{inspect pid}.")
+        Logger.info("Started lobby supervisor for #{lobby_id} at #{inspect(pid)}.")
 
       {:error, {:already_started, pid}} ->
-        Logger.warn("Tried to start lobby #{lobby_id} which is already started at #{inspect pid}.")
+        Logger.warn(
+          "Tried to start lobby #{lobby_id} which is already started at #{inspect(pid)}."
+        )
 
       {:error, reason} ->
         Logger.error("Failed to start lobby #{lobby_id}: #{reason}")
@@ -91,10 +116,20 @@ defmodule PixelForum.Forum.LobbyManager do
     result
   end
 
-  defp start_all_lobbies!() do
-    Enum.each(Lobbies.list_lobbies(), fn %Lobby{id: lobby_id} ->
-      start_lobby_(lobby_id)
-    end)
+  @spec start_all_lobbies :: :ok | :error
+  defp start_all_lobbies() do
+    try do
+      # Avoid the DB call when running the tests, because that does not play
+      # nicely with the SQL sandbox.
+      lobbies = if Mix.env() == :test, do: [], else: Lobbies.list_lobbies()
+
+      Enum.each(lobbies, fn %Lobby{id: lobby_id} -> start_lobby_(lobby_id) end)
+      :ok
+    rescue
+      e ->
+        Logger.error("Failed to start all lobbies: #{Exception.message(e)}")
+        :error
+    end
   end
 
   defp stop_lobby_(lobby_id) do
